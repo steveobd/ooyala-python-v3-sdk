05010e475eeaae32e251ee413c83115df5c79d7f
steve olechowski <sjo@blinkfire.com>
2018-09-19 16:16:58 -0500

    setup packaging

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+PACKAGES = [
+    "ooyala-python-v3-sdk"
+]
+
+
+
+setup(name='ooyala-python-v3-sdk',
+        packages=PACKAGES,
+        version='3.0',
+        author='Jose M Franco',
+        author_email='josemfranvo@ooyala.com',
+        description="Ooyala API",
+        url='https://github.com/steveobd/ooyala-python-v3-sdk',
+        install_requires=[],
+        )
